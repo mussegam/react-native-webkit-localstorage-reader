e5d30f7a2b4a7ec9196bba3056582e94d921bf10
Mike McElligott <mikemcelligott@gmail.com>
2018-09-05 13:30:39 -0400

    added support for leveldb localstorage entries

--- android/src/main/java/io/github/wootwoot1234/WebkitLocalStorageReaderModule.java
+++ android/src/main/java/io/github/wootwoot1234/WebkitLocalStorageReaderModule.java
@@ -9,6 +9,8 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableNativeMap;
+import com.github.hf.leveldb.Iterator;
+import com.github.hf.leveldb.LevelDB;
 
 import java.io.File;
 import java.nio.charset.Charset;
@@ -25,11 +27,63 @@ public class WebkitLocalStorageReaderModule extends ReactContextBaseJavaModule {
         return "WebkitLocalStorageReader";
     }
 
+    public WritableMap getLevel() {
+        String dataDir = getReactApplicationContext().getApplicationInfo().dataDir;
+        WritableMap kv = new WritableNativeMap();
+
+        try {
+            LevelDB levelDB = LevelDB.open(dataDir + "/app_webview/Local Storage/leveldb");
+
+            Iterator iterator = levelDB.iterator();
+            int prefixCount = 10;
+
+            for (iterator.seekToFirst(); iterator.isValid(); iterator.next()) {
+                byte[] key   = iterator.key();
+                byte[] value = iterator.value();
+
+                if (key.length > prefixCount) {
+                    byte[] subkey = new byte[key.length - prefixCount];
+                    byte[] subval = new byte[value.length -1];
+
+                    for (int i = prefixCount; i < key.length; i++) {
+                        subkey[i - prefixCount] = key[i];
+                    }
+
+                    for (int i = 1; i < value.length; i++) {
+                        subval[i - 1] = value[i];
+                    }
+
+                    String sKey = new String(subkey);
+                    String sVal = new String(subval);
+                    kv.putString(sKey, sVal);
+                }
+            }
+
+            iterator.close(); // closing is a must!
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return kv;
+    }
+
     @ReactMethod
     public void get(Promise promise) {
-        WritableMap kv = new WritableNativeMap();
+        WritableMap kv = getLevel();
+        boolean hasItems = kv.keySetIterator().hasNextKey();
+
+        if (hasItems) {
+            promise.resolve(kv);
+            return;
+        }
+
         String dataDir = getReactApplicationContext().getApplicationInfo().dataDir;
+
         File localstorage = new File(dataDir + "/app_webview/Local Storage/file__0.localstorage");
+        File leveldb = new File(dataDir + "/app_webview/Local Storage/leveldb");
+
 
         if (!localstorage.exists()) {
             promise.resolve(kv);
